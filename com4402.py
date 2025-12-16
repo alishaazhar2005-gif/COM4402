be50124e7486e83f002d503cdf5c099affb66a22
alishaazhar2005-gif <alishaazhar2005@gmail.com>
2025-12-16 10:49:15 +0000

    Activity

--- com4402.py
+++ com4402.py
@@ -1,4 +1,25 @@
+# activity 1
 print("Hello,COM4402!")
 name= input("What is your name? ")
-print("Nice to meet you,",name)
+age= input ("what is your age ")
 
+print("Hello" + name +", you are "+ age +"years old")
+
+
+# activity 2
+a= float(input ("Enter first number:"))
+b= float (input("Enter second number:"))
+sum_result = a + b
+product = a * b
+print("Sum is", sum_result)
+print("Product is", product)
+
+# activity 6
+product_name = input("Enter product name: ")
+price = float(input("Enter price(number only 0,not £: "))
+quantity = int(input("Enter quantity: "))
+
+total_cost = price * quantity
+
+print("You bought", quantity, "of", product_name)
+print("Total cost is", total_cost)
